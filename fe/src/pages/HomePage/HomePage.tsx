79e57cb47baf2744edfcb4329f2eae58f8f6f653
poco <godiva140@gmail.com>
2023-11-09 13:05:55 +0900

    Refactor: HomePage 레이아웃 수정

--- fe/src/pages/HomePage/HomePage.tsx
+++ fe/src/pages/HomePage/HomePage.tsx
@@ -11,8 +11,18 @@ export const HomePage = () => {
         <S.HomePage>
           <S.TitleSection>올해 내 컷</S.TitleSection>
           <S.ButtonSection>
-            <Button title="로그인" size="large" onClick={() => navigate('/login')} />
-            <Button title="회원가입" size="large" onClick={() => navigate('signUp')} />
+            <Button
+              title="회원가입"
+              size="large"
+              state="signUp"
+              onClick={() => navigate('signUp')}
+            />
+            <Button
+              title="로그인"
+              size="large"
+              state="default"
+              onClick={() => navigate('/login')}
+            />
           </S.ButtonSection>
         </S.HomePage>
       </Layout>
