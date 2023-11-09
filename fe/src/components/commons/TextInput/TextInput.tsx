40de0693121df577998d7469bec328c116ce5654
poco <godiva140@gmail.com>
2023-11-09 12:48:24 +0900

    Refactor: TextInput 수정

--- fe/src/components/commons/TextInput/TextInput.tsx
+++ fe/src/components/commons/TextInput/TextInput.tsx
@@ -5,14 +5,22 @@ interface TextInputProps {
   type?: string;
   label: string;
   placeholder?: string;
+  value?: string | number;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
 export const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
-  ({ type, label, placeholder }, ref) => {
+  ({ type, label, placeholder, value, onChange }, ref) => {
     return (
       <S.TextInput>
         <S.Label>{label}</S.Label>
-        <S.Input type={type} placeholder={placeholder} ref={ref} />
+        <S.Input
+          type={type}
+          placeholder={placeholder}
+          ref={ref}
+          value={value}
+          onChange={onChange}
+        />
       </S.TextInput>
     );
   },
