f0128173871aade2d4afd8aab5b4675b5d8f2a0d
poco <godiva140@gmail.com>
2023-11-12 14:02:16 +0900

    Feat: 회원가입 로직 및 로고 이미지 추가

--- fe/src/pages/SignUpPage/SignUpPage.tsx
+++ fe/src/pages/SignUpPage/SignUpPage.tsx
@@ -1,8 +1,9 @@
 import { useReducer, useEffect, useState } from 'react';
 import * as S from './SignUpPageStyle';
-import { Layout, TextInput, Button } from '@components/commons';
+import { Layout, TextInput, Button, ErrorModal } from '@components/commons';
 import { formReducer } from '@services/signUp/signUp';
 import { useNavigate } from 'react-router-dom';
+import { signUpUser } from '@services/signUp/signUp';
 
 export interface FormDataState {
   id: { enteredId: string; isValid: boolean | null; isTouched: boolean };
@@ -69,58 +70,82 @@ export const SignUpPage = () => {
     navigate('/');
   };
 
+  const signUpBtnClickHandler = async () => {
+    try {
+      await signUpUser(enteredId, enteredPassword, enteredNickName);
+      navigate('/login');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
-    <Layout isHeader={true} previousPage="홈으" onClick={backBtnClickHandler}>
-      <S.SignUpPage>
-        <S.TitleSection>올해 내 컷</S.TitleSection>
-        <S.ContentsSection>
-          <S.ContentDescription>
-            회원 가입으로 올해 내 컷을 기록하고, 공유하고, 다시 볼 수 있어요.
-          </S.ContentDescription>
-          <S.InputSection>
-            <TextInput
-              label="아이디"
-              placeholder="영어 소문자, 숫자, 특수기호 -,_ 포함 5~20자"
-              value={enteredId}
-              onChange={idChangeHandler}
+    <>
+      <Layout isHeader={true} previousPage="홈으" onClick={backBtnClickHandler}>
+        <S.SignUpPage>
+          <S.TitleImg />
+          <S.ProjectLogo />
+          <S.ContentsSection>
+            <S.ContentDescription>
+              회원 가입으로 올해 내 컷을 기록하고, 공유하고, 다시 볼 수 있어요.
+            </S.ContentDescription>
+            <S.InputSection>
+              <TextInput
+                label="아이디"
+                placeholder="영어 소문자, 숫자, 특수기호 -,_ 포함 5~20자"
+                value={enteredId}
+                onChange={idChangeHandler}
+                alertText={
+                  !idIsValid &&
+                  idIsTouched &&
+                  '* 영어 소문자, 숫자, 특수기호 -,_ 포함 5~20자여야 해요.'
+                }
+              />
+              <TextInput
+                label="패스워드"
+                placeholder="영어 소문자, 숫자, 특수기호 -,_ 포함 5~20자"
+                type="password"
+                value={enteredPassword}
+                onChange={passwordChangeHandler}
+                alertText={
+                  !passwordIsValid &&
+                  passwordIsTouched &&
+                  '* 영어 소문자, 숫자, 특수기호 -,_ 포함 5~20자여야 해요.'
+                }
+              />
+              <TextInput
+                label="닉네임"
+                placeholder="영어, 한글, 숫자로 3~9자"
+                value={enteredNickName}
+                onChange={nicknameChangeHandler}
+                alertText={
+                  !nicknameIsValid &&
+                  nicknameIsTouched &&
+                  '* 영어 소문자, 숫자, 특수기호 -,_ 포함 5~20자여야 해요.'
+                }
+              />
+            </S.InputSection>
+            <Button
+              title="회원가입"
+              size="large"
+              state={formDataIsValid ? 'default' : 'inactive'}
+              onClick={signUpBtnClickHandler}
             />
-            {!idIsValid && idIsTouched && (
-              <S.AlertText>영어 소문자, 숫자, 특수기호 -,_ 포함 5~20자만 가능합니다</S.AlertText>
-            )}
-            <TextInput
-              label="패스워드"
-              placeholder="영어 소문자, 숫자, 특수기호 -,_ 포함 5~20자"
-              type="password"
-              value={enteredPassword}
-              onChange={passwordChangeHandler}
-            />
-            {!passwordIsValid && passwordIsTouched && (
-              <S.AlertText>영어 소문자, 숫자, 특수기호 -,_ 포함 5~20자만 가능합니다</S.AlertText>
-            )}
-            <TextInput
-              label="닉네임"
-              placeholder="영어, 한글, 숫자로 3~9자"
-              value={enteredNickName}
-              onChange={nicknameChangeHandler}
-            />
-            {!nicknameIsValid && nicknameIsTouched && (
-              <S.AlertText>영어, 한글, 숫자로 3~9자만 가능합니다</S.AlertText>
-            )}
-          </S.InputSection>
-          <Button title="회원가입" size="large" state={formDataIsValid ? 'default' : 'inactive'} />
-          <S.TermsOfService>
-            계속 진행하면 올해 내 컷{' '}
-            <S.Link href="https://www.naver.com" target="_blank" rel="noopener noreferrer">
-              서비스 이용약관
-            </S.Link>{' '}
-            및{' '}
-            <S.Link href="https://www.naver.com" target="_blank" rel="noopener noreferrer">
-              개인정보처리방침
-            </S.Link>{' '}
-            동의한 것으로 간주됩니다.
-          </S.TermsOfService>
-        </S.ContentsSection>
-      </S.SignUpPage>
-    </Layout>
+            <S.TermsOfService>
+              계속 진행하면 올해 내 컷{' '}
+              <S.Link href="https://www.naver.com" target="_blank" rel="noopener noreferrer">
+                서비스 이용약관
+              </S.Link>{' '}
+              및{' '}
+              <S.Link href="https://www.naver.com" target="_blank" rel="noopener noreferrer">
+                개인정보처리방침
+              </S.Link>{' '}
+              동의한 것으로 간주됩니다.
+            </S.TermsOfService>
+          </S.ContentsSection>
+        </S.SignUpPage>
+      </Layout>
+      <ErrorModal title="아이디" message="비번" hasCancelBtn={false} buttonName="확인" />
+    </>
   );
 };
