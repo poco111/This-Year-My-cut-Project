02cd947e12d99693074690a7aa16db15a48c01de
poco <godiva140@gmail.com>
2023-11-12 14:02:49 +0900

    Refactor: Button 컴포넌트 수정

--- fe/src/components/commons/Button/Button.tsx
+++ fe/src/components/commons/Button/Button.tsx
@@ -1,14 +1,35 @@
 import * as S from './ButtonStyle';
 import { ButtonStyleProps } from './ButtonStyle';
 
+// TODO: 클릭핸들러 이름 변경 필요
 interface ButtonProps extends ButtonStyleProps {
   title: string;
   onClick?: () => void;
+  categoryClickHandler?: (categoryIdx: number) => void;
+  postClickHandler?: (selectedCategories) => void;
 }
 
-export const Button = ({ title, state, size, onClick }: ButtonProps) => {
+export const Button = ({
+  title,
+  state,
+  size,
+  onClick,
+  categoryClickHandler,
+  postClickHandler,
+}: ButtonProps) => {
+  const btnClickHandler = onClick
+    ? onClick
+    : (params) => {
+        if (categoryClickHandler) {
+          categoryClickHandler(params);
+        }
+        if (postClickHandler) {
+          postClickHandler(params);
+        }
+      };
+
   return (
-    <S.Button onClick={onClick} state={state} size={size}>
+    <S.Button onClick={btnClickHandler} state={state} size={size}>
       {title}
     </S.Button>
   );
