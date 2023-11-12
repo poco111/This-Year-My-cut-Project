1f1be1e6dd51c1d0b024541845d3a16cd8709a2f
poco <godiva140@gmail.com>
2023-11-12 14:18:05 +0900

    Feat: Page 컴포넌트, 공통 컴포넌트 index에 추가

--- fe/src/components/commons/index.ts
+++ fe/src/components/commons/index.ts
@@ -4,5 +4,6 @@ import { Layout } from './Layout/Layout';
 import { TextInput } from './TextInput/TextInput';
 import { Portal } from './Portal/Portal';
 import { WriteModal } from './WriteModal/WriteModal';
+import { AlertModal } from './AlertModal/AlertModal';
 
-export { Button, Icon, Layout, TextInput, Portal, WriteModal };
+export { Button, Icon, Layout, TextInput, Portal, WriteModal, AlertModal };
